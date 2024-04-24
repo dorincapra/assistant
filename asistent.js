5fabf624ea3c1b89793cd8390d5e1584b427aca4
dorincapra <66207234+dorincapra@users.noreply.github.com>
2024-04-24 16:46:19 +0300

    Add files via upload

---
+++ asistent.js
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+const asstID = "asst_qVaITkJsmfVNGzdv05Rm2izr";
+
+const thread = await openai.beta.threads.create(
+  "sk-proj-YFDdZNSnt5oHh1ERzO0UT3BlbkFJMklZUu84oihHDDYEA4W6"
+);
+console.log(thread);
+const message = await openai.beta.threads.messages.create(thread.id, {
+  role: "user",
+  content: "Zi-mi cum te cheama",
+});
+
+const run = openai.beta.threads.runs
+  .stream(thread.id, {
+    assistant_id: asstID,
+  })
+  .on("textCreated", (text) => process.stdout.write("\nassistant > "))
+  .on("textDelta", (textDelta, snapshot) =>
+    process.stdout.write(textDelta.value)
+  )
+  .on("toolCallCreated", (toolCall) =>
+    process.stdout.write(`\nassistant > ${toolCall.type}\n\n`)
+  )
+  .on("toolCallDelta", (toolCallDelta, snapshot) => {
+    if (toolCallDelta.type === "file_search") {
+      if (toolCallDelta.code_interpreter.input) {
+        process.stdout.write(toolCallDelta.code_interpreter.input);
+      }
+      if (toolCallDelta.code_interpreter.outputs) {
+        process.stdout.write("\noutput >\n");
+        toolCallDelta.code_interpreter.outputs.forEach((output) => {
+          if (output.type === "logs") {
+            process.stdout.write(`\n${output.logs}\n`);
+          }
+        });
+      }
+    }
+  });
