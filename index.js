d05e9f1fef439259e68671c31be6cf2022bc1847
dorincapra <66207234+dorincapra@users.noreply.github.com>
2024-04-25 10:44:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import { OpenAI } from "openai";
 import path from "path";
+import { fileURLToPath } from "url";
 
+// Define __dirname in ES module scope
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = process.env.PORT || 3000; // Use the PORT environment variable if provided, otherwise default to 3000
 
 app.use(express.urlencoded({ extended: true }));
-
 
 // Allow CORS
 app.use((req, res, next) => {
@@ -21,48 +23,42 @@ app.use((req, res, next) => {
 
 // Main page
 app.get("/", (req, res) => {
-res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 let thread = null;
 
 app.post("/assistant", async (req, res) => {
-  // Send the user's message first
-
   const userInput = req.body.userInput;
   try {
     const openai = new OpenAI();
     const asstID = "asst_qVaITkJsmfVNGzdv05Rm2izr";
 
-    // Creați un fir nou doar dacă nu există deja unul
     if (!thread) {
       thread = await openai.beta.threads.create();
     }
 
-    // Adăugați mesajul utilizatorului la firul existent
     const message = await openai.beta.threads.messages.create(thread.id, {
       role: "user",
       content: userInput,
     });
+
     const run = openai.beta.threads.runs
       .stream(thread.id, {
         assistant_id: asstID,
       })
       .on("textCreated", (text) => res.write("\nassistant > "))
       .on("textDelta", (textDelta, snapshot) => res.write(textDelta.value))
-      // .on("toolCallCreated", (toolCall) =>
-      //   res.write(`\nassistant > ${toolCall.type}\n\n`)
-      // )
       .on("toolCallDelta", (toolCallDelta, snapshot) => {
         if (toolCallDelta.type === "file_search") {
           if (toolCallDelta.file_search.input) {
-            // res.write(toolCallDelta.file_search.input);
+            // Add appropriate handling
           }
           if (toolCallDelta.file_search.outputs) {
             res.write("\noutput >\n");
             toolCallDelta.file_search.outputs.forEach((output) => {
               if (output.type === "logs") {
-                // res.write(`\n${output.logs}\n`);
+                // Add appropriate handling
               }
             });
           }
@@ -74,8 +70,6 @@ app.post("/assistant", async (req, res) => {
   }
 });
 
-
 app.listen(port, () => {
-  console.log(`Server listening at http://your-droplet-ip:${port}`); // Replace "your-droplet-ip" with your server's public IP address or domain name
+  console.log(`Server listening at http://your-droplet-ip:${port}`);
 });
-
