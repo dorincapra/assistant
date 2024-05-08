65c16cfef3cd14cf25b95db58ec336b8298729c9
Dorin Capra <dorinandrei16@gmail.com>
2024-05-08 11:01:28 +0300

    throwback

--- public/embedChat.js
+++ public/embedChat.js
@@ -14,7 +14,7 @@ function embedChat(serverUrl) {
       // Fetch and append the CSS
       const cssLink = document.createElement("link");
       cssLink.rel = "stylesheet";
-      cssLink.href = `http://myprojects.ro/styles.css`;
+      cssLink.href = `https://myprojects.ro/styles.css`;
       document.head.appendChild(cssLink);
 
       // Fetch and append the JavaScript
